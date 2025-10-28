6ab58c78072ced1c21c3390b5c4f210b1cee0a51
shishirgajurel <shishirgajurel999@gmail.com>
2025-10-28 11:38:55 +0100

    CPHLogicServer

---
+++ CPHLogicServer/src/test/java/org/example/cphlogicserver/CphLogicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cphlogicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CphLogicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
