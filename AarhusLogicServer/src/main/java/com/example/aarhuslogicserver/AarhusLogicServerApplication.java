b1ed7dbde1e6d03e8ded690d4a0a1ad8e7205025
Nancy <148770109+NancyNancy07@users.noreply.github.com>
2025-10-28 11:25:51 +0100

    setup

---
+++ AarhusLogicServer/src/main/java/com/example/aarhuslogicserver/AarhusLogicServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aarhuslogicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarhusLogicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AarhusLogicServerApplication.class, args);
+    }
+
+}
---
+++ AarhusLogicServer/src/test/java/com/example/aarhuslogicserver/AarhusLogicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aarhuslogicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AarhusLogicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
