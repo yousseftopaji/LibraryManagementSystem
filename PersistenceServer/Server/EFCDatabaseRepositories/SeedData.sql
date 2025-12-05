1e9c35bbb4924f82949f19ac43cccd3e8372b1d7
yousseftopaji <youssefftopaji@gmail.com>
2025-12-05 14:03:02 +0100

    deleted SeedData.sql

--- PersistenceServer/Server/EFCDatabaseRepositories/SeedData.sql
+++
@@ -1,6 +0,0 @@
-﻿-- Seed data for testing the Book-Genre relationship
-
--- Insert Users
-INSERT INTO [User] (username, passwordhash, role, name, phonenumber, email)  VALUES
-('john_doe', 'hashed_password_1', 'User', 'John Doe', '123-456-7890', 'john@g.c'),
-('jane_smith', 'hashed_password_2', 'Admin', 'Jane Smith', '987-654-3210', 'jane@g.c');
