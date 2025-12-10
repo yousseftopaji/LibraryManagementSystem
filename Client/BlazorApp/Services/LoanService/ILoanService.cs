43c618605f6b085ebfedebb07b2240257185cddd
harshit12333 <sharmaharshit4156@gmail.com>
2025-12-10 15:26:33 +0100

    extend
    extend

--- Client/BlazorApp/Services/LoanService/ILoanService.cs
+++ Client/BlazorApp/Services/LoanService/ILoanService.cs
@@ -6,4 +6,5 @@ namespace BlazorApp.Services.LoanService;
 public interface ILoanService
 {
     public Task<LoanDTO> CreateLoanAsync(CreateLoanDTO createLoanDTO);
+    Task<bool> ExtendLoanAsync(int loanId);
 }
